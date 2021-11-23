f9f9de46c0d8217512eb426bae09f8219503a2a0
FrederikBA <54848880+FrederikBA@users.noreply.github.com>
2021-11-23 15:15:26 +0100

    external api search function

---
+++ src/main/java/rest/RecipeResource.java
@@ -0,0 +1,34 @@
+package rest;
+
+import com.google.gson.Gson;
+import dtos.externalAPIs.*;
+import utils.HttpUtils;
+
+import javax.ws.rs.*;
+import javax.ws.rs.core.Context;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.UriInfo;
+import java.io.IOException;
+
+@Path("/recipes")
+public class RecipeResource {
+
+    Gson gson = new Gson();
+
+    @Context
+    private UriInfo context;
+
+    @GET
+    @Produces({MediaType.APPLICATION_JSON})
+    public String showMessage() {
+        return "{\"msg\":\"This is the recipe API section\"}";
+    }
+
+
+    @GET
+    @Produces(MediaType.APPLICATION_JSON)
+    @Path("/{ingredient}")
+    public String getRecipe(@PathParam("ingredient") String ingredient) throws IOException {
+        return HttpUtils.fetchData("https://api.spoonacular.com/recipes/complexSearch?apiKey=2d511fa06f8f41a4b083ee7f70f8f40d&includeIngredients=" + ingredient);
+    }
+}
