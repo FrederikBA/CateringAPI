7f2ccf5ed0cf8ae418203ddb24f76e7caccf8fc8
FrederikBA <54848880+FrederikBA@users.noreply.github.com>
2021-11-24 12:03:51 +0100

    unit tests for getAll + getById

--- src/test/java/facades/MenuFacadeTest.java
+++ src/test/java/facades/MenuFacadeTest.java
@@ -42,11 +42,22 @@ class MenuFacadeTest {
     @BeforeEach
     public void setUp() {
         EntityManager em = emf.createEntityManager();
-        
+        m1 = new Menu();
+        m2 = new Menu();
+
+        c1 = new Course("Ratatoullie", "france.fr", 533);
+        c2 = new Course("Goulasch", "hungary.hu", 534);
+        c3 = new Course("Fish n Chips", "england.uk", 535);
+
+        m1.addToMenu(c1);
+        m1.addToMenu(c2);
+        m2.addToMenu(c3);
 
         em.getTransaction().begin();
         em.createQuery("delete from Course").executeUpdate();
         em.createQuery("delete from Menu").executeUpdate();
+        em.persist(m1);
+        em.persist(m2);
         em.getTransaction().commit();
     }
 
@@ -67,8 +78,20 @@ class MenuFacadeTest {
         int expected = facade.createMenu(new MenuDTO(m3)).getCourses().size();
 
         assertEquals(actual, expected);
-
     }
 
+    //TODO: fix test when DTO has been fixed
+    @Test
+    public void getByIdTest() {
+        int expected = 0;
+        int actual = facade.getById(m1.getId()).getId();
+        assertEquals(expected, actual);
+    }
 
+    @Test
+    public void getAllTest() {
+        int expected = 2;
+        int actual = facade.getAll().getMenus().size();
+        assertEquals(expected, actual);
+    }
 }
