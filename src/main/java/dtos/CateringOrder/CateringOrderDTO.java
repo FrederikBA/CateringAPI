7f17e58b7be5d50ad7299db9ae0805ee36b60245
FrederikBA <54848880+FrederikBA@users.noreply.github.com>
2021-11-25 12:14:52 +0100

    update DTOs and initiated OrderFacade

--- src/main/java/dtos/CateringOrder/CateringOrderDTO.java
+++ src/main/java/dtos/CateringOrder/CateringOrderDTO.java
@@ -1,20 +1,37 @@
 package dtos.CateringOrder;
 
+import dtos.Menu.MenuDTO;
+import dtos.Role.RoleDTO;
 import entities.CateringOrder;
+import entities.Role;
 
 import java.util.Date;
+import java.util.List;
 import java.util.Objects;
+import java.util.stream.Collectors;
 
 public class CateringOrderDTO {
+    private int id;
     private Date created;
     private String deliveryDate;
+    private MenuDTO menuDTO;
+
+    public static List<CateringOrderDTO> getFromList(List<CateringOrder> orders) {
+        return orders.stream()
+                .map(order -> new CateringOrderDTO(order))
+                .collect(Collectors.toList());
+    }
 
     public CateringOrderDTO(CateringOrder cateringOrder) {
+        this.id = cateringOrder.getId();
         this.created = cateringOrder.getCreated();
         this.deliveryDate = cateringOrder.getDeliveryDate();
+        this.menuDTO = new MenuDTO(cateringOrder.getMenu());
+
     }
 
     public CateringOrderDTO(Date created, String deliveryDate) {
+        int id = -1;
         this.created = created;
         this.deliveryDate = deliveryDate;
     }
@@ -37,23 +54,24 @@ public class CateringOrderDTO {
 
     @Override
     public boolean equals(Object o) {
-
         if (this == o) return true;
         if (o == null || getClass() != o.getClass()) return false;
         CateringOrderDTO that = (CateringOrderDTO) o;
-        return Objects.equals(created, that.created) && Objects.equals(deliveryDate, that.deliveryDate);
+        return id == that.id && Objects.equals(created, that.created) && Objects.equals(deliveryDate, that.deliveryDate) && Objects.equals(menuDTO, that.menuDTO);
     }
 
     @Override
     public int hashCode() {
-        return Objects.hash(created, deliveryDate);
+        return Objects.hash(id, created, deliveryDate, menuDTO);
     }
 
     @Override
     public String toString() {
         return "CateringOrderDTO{" +
-                "created=" + created +
+                "id=" + id +
+                ", created=" + created +
                 ", deliveryDate='" + deliveryDate + '\'' +
+                ", menuDTO=" + menuDTO +
                 '}';
     }
 }
---
+++ src/main/java/facades/OrderFacade.java
@@ -0,0 +1,31 @@
+package facades;
+
+import dtos.CateringOrder.CateringOrderDTO;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+
+public class OrderFacade {
+    private static EntityManagerFactory emf;
+    private static OrderFacade instance;
+
+    private OrderFacade() {
+    }
+
+    public static OrderFacade getOrderFacade(EntityManagerFactory _emf) {
+        if (instance == null) {
+            emf = _emf;
+            instance = new OrderFacade();
+        }
+        return instance;
+    }
+
+    public CateringOrderDTO createOrder() {
+        EntityManager em = emf.createEntityManager();
+        try {
+            return null;
+        } finally {
+            em.close();
+        }
+    }
+}
