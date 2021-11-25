a075626a14d9f339f7e0e46f00da631c0c741db3
FrederikBA <54848880+FrederikBA@users.noreply.github.com>
2021-11-25 14:18:34 +0100

    create order api

--- src/main/java/rest/OrderResource.java
+++ src/main/java/rest/OrderResource.java
@@ -3,7 +3,6 @@ package rest;
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
 import dtos.CateringOrder.CateringOrderDTO;
-import entities.CateringOrder;
 import facades.OrderFacade;
 import utils.EMF_Creator;
 
